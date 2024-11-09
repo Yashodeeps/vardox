328514a145a66e86cd5e680c16d129fb9bd20a8c
Yashodeep Nimbekar <117455443+Yashodeeps@users.noreply.github.com>
2024-11-09 15:43:33 +0530

    feat: integrate wallet connection and enhance issuer page with dialog and input fields

--- src/app/(app)/td/issuer/page.tsx
+++ src/app/(app)/td/issuer/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import React from "react";
-import { Button, Timeline } from "flowbite-react";
-import { HiArrowNarrowRight } from "react-icons/hi";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { PartyPopper } from "lucide-react";
+
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 const Page = () => {
+  const { connectors, connect } = useConnect();
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  console.log(isConnected);
+
   return (
     <div className="flex justify-center w-full items-center h-screen   mx-4    ">
       <div className="border border-gray-500 rounded-xl shadow-xl w-1/2 md:w-1/3 h-2/3 ">
@@ -11,52 +30,80 @@ const Page = () => {
           Issue a certificate{" "}
         </div>
         <Separator className="bg-gray-500" />
+        <div className=" p-4  ">
+          <Dialog>
+            <DialogTrigger asChild>
+              {isConnected ? (
+                <Button variant={"secondary"}>{address}</Button>
+              ) : (
+                <Button>Connect your wallet</Button>
+              )}
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+              <DialogHeader>
+                <DialogTitle>Choose your wallet</DialogTitle>
+              </DialogHeader>
+              <div className="flex items-center flex-wrap gap-3">
+                {connectors.map((connector) => (
+                  <Button
+                    variant={"secondary"}
+                    key={connector.uid}
+                    onClick={() => connect({ connector })}
+                  >
+                    {connector.name}
+                  </Button>
+                ))}{" "}
+              </div>
+            </DialogContent>
+          </Dialog>
+          <div className="pt-3">
+            <Label>Authority</Label>
+            <Input
+              type="text"
+              placeholder="Hack this fall"
+              className="w-full p-2 rounded-lg border border-gray-500"
+            />
+          </div>
+          <div className="pt-3">
+            <Label>Type of certificate</Label>
+            <Input
+              type="text"
+              placeholder="HTF Winner"
+              className="w-full p-2 rounded-lg border border-gray-500"
+            />
+          </div>
+          <div className="flex  gap-4">
+            <div className="pt-3">
+              <Label>Receiptent vardox Id</Label>
+              <Input
+                type="text"
+                placeholder="IR23456"
+                className=" p-2 rounded-lg border border-gray-500"
+              />
+            </div>
+            <div className="pt-3">
+              <Label>Upload doc</Label>
+              <Input
+                type="file"
+                className=" p-2 rounded-lg border border-gray-500"
+              />
+            </div>
+          </div>
+          <div className="pt-3">
+            <Label>Doc hash</Label>
+            <Input
+              type="text"
+              placeholder="Upload doc to get hash"
+              className=" p-2 rounded-lg border border-gray-500"
+              disabled
+            />
+          </div>
 
-        <Timeline>
-          <Timeline.Item>
-            <Timeline.Point />
-            <Timeline.Content>
-              <Timeline.Time>February 2022</Timeline.Time>
-              <Timeline.Title>
-                Application UI code in Tailwind CSS
-              </Timeline.Title>
-              <Timeline.Body>
-                Get access to over 20+ pages including a dashboard layout,
-                charts, kanban board, calendar, and pre-order E-commerce &
-                Marketing pages.
-              </Timeline.Body>
-              <Button color="gray">
-                Learn More
-                <HiArrowNarrowRight className="ml-2 h-3 w-3" />
-              </Button>
-            </Timeline.Content>
-          </Timeline.Item>
-          <Timeline.Item>
-            <Timeline.Point />
-            <Timeline.Content>
-              <Timeline.Time>March 2022</Timeline.Time>
-              <Timeline.Title>Marketing UI design in Figma</Timeline.Title>
-              <Timeline.Body>
-                All of the pages and components are first designed in Figma and
-                we keep a parity between the two versions even as we update the
-                project.
-              </Timeline.Body>
-            </Timeline.Content>
-          </Timeline.Item>
-          <Timeline.Item>
-            <Timeline.Point />
-            <Timeline.Content>
-              <Timeline.Time>April 2022</Timeline.Time>
-              <Timeline.Title>
-                E-Commerce UI code in Tailwind CSS
-              </Timeline.Title>
-              <Timeline.Body>
-                Get started with dozens of web components and interactive
-                elements built on top of Tailwind CSS.
-              </Timeline.Body>
-            </Timeline.Content>
-          </Timeline.Item>
-        </Timeline>
+          <Button className="bg-purple-600 hover:bg-purple-500 flex gap-3 my-3">
+            Issue
+            <PartyPopper />{" "}
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/app/utils/Providers.tsx
+++ src/app/utils/Providers.tsx
@@ -3,9 +3,25 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { store } from "@/redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { Provider } from "react-redux";
+import { WagmiProvider } from "wagmi";
+
+import { http, createConfig } from "wagmi";
+import { base, mainnet, optimism } from "wagmi/chains";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [injected(), metaMask()],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+});
+const queryClient = new QueryClient();
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -18,7 +34,11 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
       >
         <SessionProvider>
           <Provider store={store}>
-            {children}
+            <WagmiProvider config={config}>
+              <QueryClientProvider client={queryClient}>
+                {children}
+              </QueryClientProvider>
+            </WagmiProvider>
             <Toaster />
           </Provider>
         </SessionProvider>
