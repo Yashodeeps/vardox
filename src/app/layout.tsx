141e87c7607793afee1e218ef02f5470cbd4fbfd
Yashodeep Nimbekar <117455443+Yashodeeps@users.noreply.github.com>
2024-11-11 20:44:59 +0530

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Providers from "./utils/Providers";
 import Navbar from "@/components/custom/Navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Providers>
           <Navbar />
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
